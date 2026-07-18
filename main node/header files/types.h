0fd97b2019d9ea57edad7a0562e8533a446b28e0
Rahulrapelli <RAPELLIRAHUL246@GMAIL.COM>
2026-07-18 21:55:03 +0530

    Update types.h

--- main node/header files/types.h
+++ main node/header files/types.h
@@ -1,7 +1,8 @@
-typedef unsigned char u8;
-typedef signed char s8;
-typedef unsigned short int u16;
-typedef signed short int s16;
-typedef unsigned int u32;
-typedef signed int s32;
-typedef float f32;
+typedef unsigned char u8;            // Unsigned 8-bit integer (0 to 255)
+typedef signed char s8;                // Signed 8-bit integer (-128 to 127)
+typedef unsigned short int u16;          // Unsigned 16-bit integer (0 to 65,535)
+typedef signed short int s16;            // Signed 16-bit integer (-32,768 to 32,767)
+typedef unsigned int u32;               // Unsigned 32-bit integer (0 to 4,294,967,295)
+typedef signed int s32;                // Signed 32-bit integer (-2,147,483,648 to 2,147,483,647)
+typedef float f32;                      // 32-bit floating-point data type
+
